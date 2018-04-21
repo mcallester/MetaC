508c127013d447abae0033b08c37e14bebc34009
David McAllester <mcallester@ttic.edu>
2018-04-21 09:06:40 -0500

    trivial

---
+++ base_decls.h
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <setjmp.h>
+#include <time.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/file.h>
+#include <fcntl.h>
+#include <string.h>
+#include <dlfcn.h>
+
+typedef char * charptr;
+typedef void * voidptr;
+typedef voidptr * voidptrptr;
+typedef FILE * FILEptr;
+
+typedef struct pliststruct{
+  void * key;
+  struct expstruct * data;
+  struct pliststruct * rest;
+}pliststruct,*plist;
+
+typedef struct expstruct{
+  plist data;
+  char constructor;
+  struct expstruct * arg1;
+  struct expstruct * arg2;
+}expstruct,*expptr;
+
+/** ========================================================================
+errors and breaks
+========================================================================**/
+
+void cbreak();
+
+void berror(charptr s);
+
+void uerror(expptr e);
+
+/** ========================================================================
+interning (procedures called by the expansion of backquote)
+========================================================================**/
+
+expptr intern_exp(char constr, expptr arg1, expptr arg2);
+
+charptr intern_string(charptr s);
+
+/** ========================================================================
+case
+
+Case needs constructor, arg1, and arg2 which are all preprocessor inlines.
+It also uses case_error
+========================================================================**/
+
+char constructor(expptr e);
+expptr arg1(expptr e);
+expptr arg2(expptr e);
+void case_error(expptr topexp);
+
+/** ========================================================================
+properties of expressions
+========================================================================**/
+
+expptr getprop(expptr e, expptr key, expptr defaultval);
+void setprop(expptr e, expptr key, expptr val);
+void addprop(expptr e, expptr key, expptr val);
+
+/** ========================================================================
+conversions
+========================================================================**/
+
+charptr exp_string(expptr s);
+
+expptr string_symbol(charptr s);
+
+// expptr string_exp(charptr s);
+
+expptr int_exp(int i);
+
+int symbol_int(expptr s);
+
+/** ========================================================================
+gensym
+========================================================================**/
+
+expptr gensym(charptr s);
+
+
+/** ========================================================================
+reading and printing
+========================================================================**/
+
+expptr read_from_terminal();
+
+void open_input_file(charptr s);
+
+expptr read_from_file();
+
+void open_output_file(charptr s);
+
+void pprint(expptr e, FILEptr f, int i);
+
+/** ========================================================================
+macros
+========================================================================**/
+
+void set_umacro(charptr s, expptr f(expptr));
+
+expptr macroexpand(expptr e);
+
+expptr macroexpand1(expptr e);
+
+/** ========================================================================
+expression walking
+========================================================================**/
+
+int atomp(expptr e);
+
+int symbolp(expptr e);
+
+//expptr mapargs(exptr f(exptr), e expptr);
+
+/** ========================================================================
+cons and nil 
+========================================================================**/
+
+expptr cons(expptr x, expptr y);  //abbreviates `{{$x},$y}
+
+typedef  expptr exp_to_exp(expptr);
+
+expptr mapcar(exp_to_exp f, expptr l);
+
+/** ========================================================================
+undo frames
+========================================================================**/
+
+void push_undo_frame();
+
+void pop_undo_frame();
+
+voidptr undo_alloc(int size);
+
+/** ========================================================================
+stack frames
+========================================================================**/
+
+voidptr stack[STACK_DIM];
+void push_stack_frame();
+void push_stack_frame2(voidptr frame);
+void pop_stack_frame();
+
+voidptr current_frame();
+
+voidptr stack_alloc(int size);
+
+void mcerror(expptr e);
